03ad9dd68dcc0441ef3063f3574f47e9029115e9
János Farkas <farkas48@uniba.sk>
2016-09-09 15:46:09 +0200

    Update tasks.py

--- ckanext/background_api_call/tasks.py
+++ ckanext/background_api_call/tasks.py
@@ -34,36 +34,23 @@ def call_function(context, data_dict):
         funct = data_dict.get('function')
     except KeyError:
         funct = "" 
-    #logging.error(funct)
-    #toolkit.get_action(funct)(context, data_dict)
-    #logging.error(context)
 
     api_url = urlparse.urljoin(site_url, 'api/3/action')
-    logging.error("api url ... "+api_url)
     url = api_url + '/'+data_dict['function']
-    #logging.error(url)
     fil = data_dict.get("file", None)
-    #logging.error( "file in data dict" + fil)
     data_dict.pop("function")
-    #logging.error(data_dict)
     upload_files = []
     up = data_dict.pop("file")
     if fil != None:
-        logging.error("file not null ... OK")
         file_to_upload = open(up, 'rb+')
         upload_files = [('upload', file_to_upload)]
         
-    #logging.error( "file in data dict")
-    
-    #data_dict.pop("upload")
-    #logging.error(data_dict)
     response = requests.post(
         url,
         data=data_dict,
         headers={'Authorization': context['apikey']}, 
         files=upload_files)
         
-    logging.error("response ... OK ... "+ response.status_code)
         
     if response.status_code == 200:
         to = {}
@@ -76,26 +63,19 @@ def call_function(context, data_dict):
         to["result"] = 'task failed...'
         to['response'] = json.loads(response.text)
 
-
     dd = {'to':json.dumps(to), 'id':data_dict['task_id']}
-    response2 = requests.post(
+    requests.post(
         api_url + '/change_db_row',
         json.dumps(dd),
         headers={'Authorization': context['apikey'],
                  'Content-Type': 'application/json'}
     )
-    logging.error("change db row done ...  OK")
-    logging.error("status code ... "+response2.status_code)
     if funct == "resource_create" or funct == "resource_update":
-        logging.error("funct ... "+ funct)
-        request3 = requests.post(
+        requests.post(
             api_url + '/resource_update',
             json.dumps({"id":to["response"]["result"]["id"], "url":data_dict["file_name"]}),
             headers={'Authorization': context['apikey'],
                      'Content-Type': 'application/json'})
-        logging.error("change url ... OK")
-        logging.error("change url status code ... "+request3.status_code)
-        logging.error("change url response ... "+request3.text)
     
     import os
     os.remove(fil)
