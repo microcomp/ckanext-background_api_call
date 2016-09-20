2644c26c6acc182ee26471bc8d487af7d10617a2
János Farkas <farkas48@uniba.sk>
2016-09-20 11:07:25 +0200

    Update tasks.py

--- ckanext/background_api_call/tasks.py
+++ ckanext/background_api_call/tasks.py
@@ -16,15 +16,18 @@ site_url = config.get("ckan.internal_site_url","http://127.0.0.1")
 def call_function(context, data_dict):
     context = json.loads(context)
     api_url = urlparse.urljoin(site_url, 'api/3/action')
+    logging.debug("background_api_call: ")
+    logging.debug(data_dict)
     requests.post(
         api_url + '/change_db_row',
-        json.dumps( {'to':json.dumps({ 'result':'task started...'}), 'id':data_dict['task_id']} ),
+        data = json.dumps({'to':json.dumps({ 'result':'task started...'}), 'id':data_dict['task_id']}) ,
         headers={'Authorization': context['apikey'],
-                 'Content-Type': 'application/json'}
+                 'content-type': 'application/json'}
     )
     
-    logging.error(context)
-    logging.error("celery task started...")
+    logging.debug("background_api_call: ")
+    logging.debug(context)
+    logging.debug("background_api_call: "+"celery task started...")
     funct = ""
     try:
         funct = data_dict.get('function')
@@ -36,17 +39,20 @@ def call_function(context, data_dict):
     fil = data_dict.get("file", None)
     data_dict.pop("function")
     upload_files = []
-    up = data_dict.pop("file")
-    if fil != None:
+    response = None
+    try:
+        up = data_dict.pop("file")
         file_to_upload = open(up, 'rb+')
         upload_files = [('upload', file_to_upload)]
-        
-    response = requests.post(
-        url,
-        data=data_dict,
-        headers={'Authorization': context['apikey']}, 
-        files=upload_files)
-        
+        logging.debug("background_api_call: "+"upload...")
+        response = requests.post(url,data=data_dict,headers={'Authorization': context['apikey']}, files=upload_files)
+    except KeyError:
+        logging.debug("background_api_call: "+"no upload...")
+        logging.debug("background_api_call: ")
+        logging.debug(data_dict)
+        response = requests.post(url,data=json.dumps(data_dict) ,headers={'Authorization': context['apikey'], 'content-type': 'application/json'})
+        logging.debug("background_api_call: "+"request done...")
+
         
     if response.status_code == 200:
         to = {}
@@ -60,22 +66,26 @@ def call_function(context, data_dict):
         to['response'] = json.loads(response.text)
 
     dd = {'to':json.dumps(to), 'id':data_dict['task_id']}
+    logging.debug("background_api_call: "+"changing db row...")
     requests.post(
         api_url + '/change_db_row',
-        json.dumps(dd),
+        data=json.dumps(dd),
         headers={'Authorization': context['apikey'],
                  'Content-Type': 'application/json'}
     )
+    logging.debug("background_api_call: "+"changing db done...")
     if funct == "resource_create" or funct == "resource_update":
+        logging.debug("background_api_call: "+"im here...")
         requests.post(
             api_url + '/resource_update',
-            json.dumps({"id":to["response"]["result"]["id"], "url":data_dict["file_name"]}),
+            data=json.dumps({"id":to["response"]["result"]["id"], "url":data_dict["file_name"]}),
             headers={'Authorization': context['apikey'],
-                     'Content-Type': 'application/json'})
+                     'content-type': 'application/json'})
     
     import os
-    os.remove(fil)
-    dr = fil.split("/")
-    dr = dr[:-1]
-    os.rmdir("/".join(dr))
+    if fil != None:
+        os.remove(fil)
+        dr = fil.split("/")
+        dr = dr[:-1]
+        os.rmdir("/".join(dr))
     return "done..."
